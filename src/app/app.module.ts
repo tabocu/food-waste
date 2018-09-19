73e12c00dd09f0e1a134fe43c1831541c05e5600
Tales <tabocu@gmail.com>
2018-09-19 16:59:46 -0300

    Removed unused tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { AlimentosPage } from '../pages/alimentos/alimentos'
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -15,10 +13,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     AlimentosPage,
+    AboutPage,
     TabsPage
   ],
   imports: [
@@ -28,10 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     AlimentosPage,
+    AboutPage,
     TabsPage
   ],
   providers: [
