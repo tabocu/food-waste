add64d4ed7a8f4e01f14e49a47a9580cfa0ac8b2
Tales <tabocu@gmail.com>
2018-10-05 14:18:45 -0300

    Created Preços page

---
+++ src/pages/precos/precos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PrecosPage } from './precos';
+
+@NgModule({
+  declarations: [
+    PrecosPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PrecosPage),
+  ],
+})
+export class PrecosPageModule {}
