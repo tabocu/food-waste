c151f531e10f6b067a7d800b879916bb309a2c0b
Tales <tabocu@gmail.com>
2018-10-05 22:39:23 -0300

    Created PrecoModel

--- src/models/receita/receita.ts
+++ src/models/receita/receita.ts
@@ -21,9 +21,25 @@ export
 
   clone(): ReceitaModel {
     let receitaModelCopy = new ReceitaModel(this.nome);
+    receitaModelCopy.key = this.key;
 
     this.quantidades.forEach((quantidade) => { receitaModelCopy.quantidades.push(quantidade.clone()) } );
 
     return receitaModelCopy;
   }
 }
+
+export
+  class PrecoModel
+  implements ClonableModel<PrecoModel> {
+
+  constructor(
+    public preco: number,
+    public alimento: number) {
+    
+  }
+
+  clone(): PrecoModel {
+    throw new Error("Method not implemented.");
+  }
+ }
