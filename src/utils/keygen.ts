1b325c89e778c54f85b75ddc2a9d0bb60a7dbd77
Tales <tabocu@gmail.com>
2018-10-26 20:48:07 -0300

    Implemented Id on keygen

--- src/utils/keygen.ts
+++ src/utils/keygen.ts
@@ -1,5 +1,6 @@
 export abstract class KeyGen<T> {
   abstract getNextKey(): Key<T>;
+  abstract getKey(id: number): Key<T>;
   abstract resetKeys();
 
   static getInvalidKey<T>() : Key<T> {
@@ -11,16 +12,29 @@ export abstract class KeyGen<T> {
   }
 }
 
-export interface Key<T> { isValid(): boolean; }
+export interface Key<T> { 
+  isValid(): boolean;
+  getId(): number;
+}
 
 class KeyGenImp<T> implements KeyGen<T> {
 
-  keySet: Set<KeyImp<T>> = new Set <KeyImp<T>>();
+  keySet: Set<KeyImp<T>> = new Set<KeyImp<T>>();
+  keyMap = {};
+  counter: number = 0;
 
   constructor() {}
 
-  getNextKey() : Key<T> {
-    let key = new KeyImp<T>(this);
+  getKey(id: number): Key<T> {
+    return (id in this.keyMap)
+       ? this.keyMap[id]
+       : new InvalidKeyImp<T>();
+  }
+
+  getNextKey(): Key<T> {
+    let key = new KeyImp<T>(this, this.counter);
+    this.keyMap[this.counter] = key;
+    this.counter++;
     this.keySet.add(key);
     return key;
   }
@@ -32,13 +46,20 @@ class KeyGenImp<T> implements KeyGen<T> {
 
 class KeyImp<T> implements Key<T> {
   
-  constructor(private keyGen: KeyGenImp<T>) {}
+  constructor(
+    private keyGen: KeyGenImp<T>,
+    private id: number) {}
 
   isValid(): boolean {
     return this.keyGen.keySet.has(this);
+  }
+
+  getId(): number {
+    return this.id;
   }
 }
 
 class InvalidKeyImp<T> implements Key<T> {
   isValid(): boolean { return false; }
+  getId(): number { return -1; }
 }
