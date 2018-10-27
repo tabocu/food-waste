0b71d347ecadd9a0979afdf19314c16ef8de089a
Tales <tabocu@gmail.com>
2018-10-27 17:21:29 -0300

    Created ResultadoModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,45 @@
+import { MyApp } from './app.component';
+
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { AlimentoPage } from '../pages/alimento/alimento'
 import { AlimentosPage } from '../pages/alimentos/alimentos'
+
 import { ReceitaPage } from '../pages/receita/receita'
 import { ReceitasPage } from '../pages/receitas/receitas'
-import { PrecosPage } from '../pages/precos/precos'
-import { PrecoPage } from '../pages/preco/preco'
-import { TabsPage } from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { PrecoPage } from '../pages/preco/preco'
+import { PrecosPage } from '../pages/precos/precos'
+
+import { QuantidadesPage } from '../pages/quantidades/quantidades';
+import { ResultadoPage } from '../pages/resultado/resultado';
+
+import { TabsPage } from '../pages/tabs/tabs';
+import { RunPage } from '../pages/run/run';
+
 import { AlimentosProvider } from '../providers/alimentos/alimentos';
 import { ReceitasProvider } from '../providers/receitas/receitas';
 import { PrecosProvider } from '../providers/precos/precos';
-import { IndexedPipe } from '../pipes/indexed/indexed';
-import { RunPage } from '../pages/run/run';
-import { QuantidadesPage } from '../pages/quantidades/quantidades';
 import { OtimizacaoProvider } from '../providers/otimizacao/otimizacao';
-import { HttpClientModule } from '@angular/common/http'; 
+
+import { IndexedPipe } from '../pipes/indexed/indexed';
 
 @NgModule({
   declarations: [
     MyApp,
-    AlimentoPage,
     AlimentosPage,
-    ReceitaPage,
+    AlimentoPage,
     ReceitasPage,
+    ReceitaPage,
     PrecosPage,
     PrecoPage,
     QuantidadesPage,
+    ResultadoPage,
     RunPage,
     TabsPage,
     IndexedPipe,
@@ -44,13 +52,14 @@ import { HttpClientModule } from '@angular/common/http';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AlimentoPage,
     AlimentosPage,
-    ReceitaPage,
+    AlimentoPage,
     ReceitasPage,
+    ReceitaPage,
     PrecosPage,
     PrecoPage,
     QuantidadesPage,
+    ResultadoPage,
     RunPage,
     TabsPage
   ],
